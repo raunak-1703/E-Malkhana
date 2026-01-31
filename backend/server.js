5183f913edb67f2568dd4e969806d4e398c360e6
Raunak Kumar <nodepoint3@gmail.com>
2026-01-31 14:22:36 +0530

    feat(database): connect backend to MongoDB using mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors'
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 
 dotenv.config()
 
@@ -17,6 +18,7 @@ app.get('/health',(req,res)=>{
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,()=>{
+app.listen(PORT, async ()=>{
+    await connectDB();
     console.log('Server is running on PORT: ',PORT);
 })
